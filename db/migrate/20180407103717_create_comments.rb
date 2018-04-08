75c08c1fd780745bcd0b3403c7bb62554e610a27
PSzyszka <pawelszyszka26@wp.pl>
2018-04-08 22:30:39 +0200

    Adding db folder to repository

---
+++ db/migrate/20180407103717_create_comments.rb
@@ -0,0 +1,13 @@
+class CreateComments < ActiveRecord::Migration[5.1]
+  def change
+    create_table :comments do |t|
+      t.text :description, null: false
+      t.references :movie, index: true, null: false
+      t.references :user, index: true, null: false
+
+      t.timestamps
+    end
+
+    add_index :comments, [:movie_id, :user_id], unique: true
+  end
+end
