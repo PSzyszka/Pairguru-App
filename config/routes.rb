1332e30d2337a00a8fc7cdbf4029babf5a864367
PSzyszka <pawelszyszka26@wp.pl>
2018-04-08 22:34:38 +0200

    Adding config folder

---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  root "home#welcome"
+  resources :genres, only: :index do
+    member do
+      get "movies"
+    end
+  end
+
+  resources :users, only: [:index]
+
+  resources :movies, only: [:index, :show] do
+    resources :comments, only: [:create, :destroy]
+    member do
+      get :send_info
+    end
+    collection do
+      get :export
+    end
+  end
+end
